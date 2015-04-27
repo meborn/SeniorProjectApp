34a3bd828c9db50ed182934addd2d6546ca4cb22
Megan Eborn <mneborn@gmail.com>
2015-04-27 14:04:47 -0600

    fix profile validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,6 +4,8 @@ class Profile < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
   validates :color, presence: true
+  validates :zip, presence: true
+  validates :zip, length: { is: 5 }
 
   has_attached_file :avatar, :styles => { :small => "150x150#", :thuumb => "80x80#" }
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
